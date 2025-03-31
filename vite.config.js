091e9de60c82bca1ae296bb7baa9843712a6e7ff
ZackCornfield <139100711+ZackCornfield@users.noreply.github.com>
2025-03-31 15:45:28 +0800

    .

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { env } from "process";
 
 export default defineConfig({
   plugins: [react()],
+  base: env.BASE_URL || "/",  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
